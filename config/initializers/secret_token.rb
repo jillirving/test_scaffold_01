2d78694790f68588de7e2b0f9f35b21e8be8471f
Jill Irving <jillirving@gmail.com>
2011-09-29 23:11:56 +0100

    Initial commit of scaffold test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestScaffold01::Application.config.secret_token = '4f13ea62fdee57926a63a53951d4f1d77c2cebd879856d4330d545062298499e8646e5299356f33f437219a250287327dabe5564f3ada613d92ed4f6009c33d6'
